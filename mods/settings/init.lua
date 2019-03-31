ed528fde66b75a3f6002d41b6d5303e1ba32860e
Ganome <ganome@gmail.com>
2019-03-31 14:20:06 -0600

    hoes.lua, added /arcade warp, switched digger<>player - need to switch back

--- mods/settings/init.lua
+++ mods/settings/init.lua
@@ -1,5 +1,6 @@
 settings = {};
 settings.spawnpos = {x=63,y=2,z=-77}
+settings.arcadepos = {x=63,y=2,z=-77}
 
 minetest.register_chatcommand("spawn", {
 	description = "Move player back to spawn",
@@ -10,5 +11,17 @@ minetest.register_chatcommand("spawn", {
 	func = function(name, param)
 		local player = minetest.get_player_by_name(name); --if player then player:set_hp(0) end
 		player:set_pos(settings.spawnpos)
+	end
+})
+
+minetest.register_chatcommand("arcade", {
+	description = "Move player back to spawn",
+	privs = {
+		interact = true
+	},
+	
+	func = function(name, param)
+		local player = minetest.get_player_by_name(name); --if player then player:set_hp(0) end
+		player:set_pos(settings.arcadepos)
 	end
 })
